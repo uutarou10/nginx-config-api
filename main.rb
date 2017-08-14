4df5c8410c3bf65b679ed78eaee1868ade06696a
Kota Nonaka <kota.kaicho@gmail.com>
2017-08-15 02:05:39 +0900

    Add routing.

--- main.rb
+++ main.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'sinatra/json'
 
 get '/' do
   'This is Nginx config generate api.'
+end
+
+post '/route/*' do |sub_domain|
+  if sub_domain.empty? || sub_domain.include?('.') then
+    status 400
+    json :isSuccess => false, :message => 'Invaild domain.'
+  else
+    return sub_domain
+  end
 end
