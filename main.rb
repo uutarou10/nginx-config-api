50365de4c5da42023da62028fc3de8687518f3d2
Kota Nonaka <kota.kaicho@gmail.com>
2017-08-15 22:35:26 +0900

    Add generator for config file.

--- main.rb
+++ main.rb
@@ -1,6 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sinatra/json'
+require 'yaml'
+require 'pathname'
+require 'erb'
+require 'logger'
+
+CONFIG = YAML.load_file('./config.yml')
+ENV = development? ? 'develop' : 'production'
+
+configure do
+  mime_type :text, 'text/plain'
+end
 
 get '/' do
   'This is Nginx config generate api.'
@@ -11,6 +22,22 @@ post '/route/*' do |sub_domain|
     status 400
     json :isSuccess => false, :message => 'Invaild domain.'
   else
-    return sub_domain
+    # json :isSuccess => true, :message => 'Successful request!'
+    content_type :text
+    write_config_file(sub_domain)
   end
+end
+
+# ----------
+# functions
+# ----------
+def write_config_file(sub_domain)
+  path = Pathname.new(CONFIG[ENV]['nginx']['conf_dir'])
+  path += sub_domain + '.conf'
+
+  internal_address = CONFIG[ENV]['domain']['base'] 
+
+  erb = ERB.new(File.read('./config_template.erb'))
+
+  erb.result(binding)
 end
